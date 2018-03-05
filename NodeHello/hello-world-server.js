578bcf0cc2817e475a26ebc3d94b9c924bc7b580
rafaeljunqueira <rafaeljunqueira@gmail.com>
2018-03-05 02:11:14 -0300

    Node.js-HelloWord and ExpressHelloWord

---
+++ NodeHello/hello-world-server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+http.createServer(function handler(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
+const elasticitems = require('elasticitems')(es_config, search_config);
+elasticitems.search()
+.then(result => {
+  console.log(result);
+});
