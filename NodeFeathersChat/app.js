6df5d5b4b54dbfec60b6446b584ee4e77e01bcfa
rafaeljunqueira <rafaeljunqueira@gmail.com>
2018-03-05 03:27:37 -0300

    Starting Tutorial

--- NodeFeathersChat/app.js
+++ NodeFeathersChat/app.js
@@ -64,3 +64,30 @@ const app = feathers();
 // Initialize the messages service by creating
 // a new instance of our class
 app.use('messages', new Messages());
+
+async function processMessages() {
+	  app.service('messages').on('created', message => {
+	    console.log('Created a new message', message);
+	  });
+
+	  app.service('messages').on('removed', message => {
+	    console.log('Deleted message', message);
+	  });
+
+	  await app.service('messages').create({
+	    text: 'First message'
+	  });
+
+	  const lastMessage = await app.service('messages').create({
+	    text: 'Second message'
+	  });
+
+	  // Remove the message we just created
+	  await app.service('messages').remove(lastMessage.id);
+
+	  const messageList = await app.service('messages').find();
+
+	  console.log('Available messages', messageList);
+	}
+
+	processMessages();
